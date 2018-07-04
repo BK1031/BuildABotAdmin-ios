fa8df4e06ef664d5e306cbbb58ff463b5b8f5956
Bharat Kathi <bharat1031@gmail.com>
2018-07-03 17:35:58 -0700

    Completed New Announcement VC

--- Build A Bot Admin/Home/CreateAlertViewController.swift
+++ Build A Bot Admin/Home/CreateAlertViewController.swift
@@ -8,22 +8,69 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateAlertViewController: UIViewController {
     
     @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var announcementTitleTextField: UITextField!
+    @IBOutlet weak var announcementDetailText: UITextView!
+    @IBOutlet weak var announcementSesionButton: UIButton!
+    
+    var ref:DatabaseReference?
+    var databaseHandle:DatabaseHandle?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
+        self.navigationItem.title = "New Announcement"
         createButton.layer.cornerRadius = 10
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        announcementSesionButton.setTitle(selectedSession, for: .normal)
+        if selectedSession.contains("2") == false {
+            announcementSesionButton.setTitle("Click to Select a Session", for: .normal)
+        }
     }
 
     @IBAction func createAnnouncement(_ sender: UIButton) {
-        //Handle announcement creation here
+        if announcementTitleTextField.text == "" {
+            let alert = UIAlertController(title: "Empty Title", message: "Please enter a title for your announcement.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Got it", style: .default, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+        
+        else if selectedSession == "blank" {
+            let alert = UIAlertController(title: "Select a Session", message: "You must select a session to post your announcement to.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Got it", style: .default) { (completion) in
+                self.performSegue(withIdentifier: "filterSession", sender: nil)
+            }
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+        
+        else if announcementDetailText.text == "Click to Edit Announcement Detail" {
+            let alert = UIAlertController(title: "Empty Announcement Body", message: "Please enter the details for your announcement.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Got it", style: .default, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+        
+        else {
+            let announcementReference = self.ref?.child("announcements").child(selectedSession).childByAutoId()
+            let announcement = ["details": announcementDetailText.text, "title": announcementTitleTextField.text, "id": announcementReference?.key] as [String: AnyObject]
+            announcementReference?.updateChildValues(announcement)
+            self.dismiss(animated: true, completion: nil)
+        }
     }
     
-    @IBAction func createAnnoncement(_ sender: UIBarButtonItem) {
+    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func announcementSession(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "filterSession", sender: self)
+    }
 }
